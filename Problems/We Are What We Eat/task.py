9e3f503c4558362d809173bc69ee2124603a3855
ushapatturaj <ushadevipatturaj@gmail.com>
2020-07-30 15:49:13 +0200

    Dictionary operations

---
+++ Problems/We Are What We Eat/task.py
@@ -0,0 +1,16 @@
+# the list "meals" is already defined
+# your code here
+meals = [
+        {"title": "Oatmeal pancakes with apple and cinnamon", "kcal": 370},
+        {"title": "Italian salad with fusilli and ham", "kcal": 320},
+        {"title": "Bulgur with vegetables", "kcal": 350},
+        {"title": "Curd souffle with lingonberries and ginger", "kcal": 225},
+        {"title": "Oatmeal with honey and peanuts", "kcal": 440}]
+calories = 0
+for meal in meals:
+    for key, value in meal.items():
+        if key == 'kcal':
+            calories += meal['kcal']
+        else:
+            continue
+print(calories)
